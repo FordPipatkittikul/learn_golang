28339d990ebabb6c4d4937de62bbeac81f7a0dd8
Nithi Pipatkittikul <fordpipatkitkul@gmail.com>
2026-03-21 21:59:52 +0700

    gorm

--- orm/main.go
+++ orm/main.go
@@ -7,6 +7,7 @@ import (
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
 	"gorm.io/gorm/logger"
 )
 
@@ -37,13 +38,57 @@ func main() {
 
 	// err = db.Migrator().CreateTable(Gender{})
 
-	// err = db.AutoMigrate(Gender{}, Test{})
+	// err = db.AutoMigrate(Gender{}, Test{}, Customer{})
 	// if err != nil {
 	// 	fmt.Printf("AutoMigrate error: %v\n", err)
 	// }
 
-	CreateGender("Female")
+	// CreateGender("Female")
+	// GetGenders()
+	// GetGender(1)
+	// GetGenderByName("Female")
+	// UpdateGender(1, "Gay")
+	// UpdateGender2(2, "Lesbian")
+	// DeleteGender(1)
 
+	// CreateTest(0, "Test1")
+	// CreateTest(0, "Test2")
+	// CreateTest(0, "Test3")
+	// SoftDeleteTest(3)
+	// HardDeleteTest(2)
+
+	// db.Migrator().CreateTable(Customer{})
+	// CreateCustomer("joke", 2)
+	GetCustomers()
+}
+
+type Customer struct {
+	ID       uint
+	Name     string
+	Gender   Gender
+	GenderID uint
+}
+
+func GetCustomers() {
+	customers := []Customer{}
+	tx := db.Preload(clause.Associations).Find(&customers)
+	if tx.Error != nil {
+		fmt.Println(tx.Error)
+		return
+	}
+	for _, customer := range customers {
+		fmt.Printf("%v|%v|%v\n", customer.ID, customer.Name, customer.Gender.Name)
+	}
+}
+
+func CreateCustomer(name string, genderID uint) {
+	customer := Customer{Name: name, GenderID: genderID}
+	tx := db.Create(&customer)
+	if tx.Error != nil {
+		fmt.Println(tx.Error)
+		return
+	}
+	fmt.Println(customer)
 }
 
 type Gender struct {
@@ -61,9 +106,69 @@ func CreateGender(name string) {
 	fmt.Println(gender)
 }
 
-// func GetGenders() {
-// 	db.
-// }
+func GetGenders() {
+	genders := []Gender{}
+	tx := db.Find(&genders)
+	if tx.Error != nil {
+		fmt.Println(tx.Error)
+		return
+	}
+	fmt.Println(genders)
+}
+
+func GetGender(id uint) {
+	gender := Gender{}
+	tx := db.First(&gender, "id=?",id)
+	if tx.Error != nil {
+		fmt.Println(tx.Error)
+		return
+	}
+	fmt.Println(gender)
+}
+
+func GetGenderByName(name string) {
+	gender := Gender{}
+	tx := db.First(&gender, "name=?", name)
+	if tx.Error != nil {
+		fmt.Println(tx.Error)
+		return
+	}
+	fmt.Println(gender)
+}
+
+func UpdateGender(id uint, name string) {
+	gender := Gender{}
+	tx := db.First(&gender, "id=?",id)
+	if tx.Error != nil {
+		fmt.Println(tx.Error)
+		return
+	}
+
+	gender.Name = name
+	tx = db.Save(&gender)
+	if tx.Error != nil {
+		fmt.Println(tx.Error)
+		return
+	}
+}
+
+func UpdateGender2(id uint, name string) {
+	gender := Gender{Name: name}
+	tx := db.Model(&Gender{}).Where("id=?", id).Updates(gender)
+	if tx.Error != nil {
+		fmt.Println(tx.Error)
+		return
+	}
+}
+
+func DeleteGender(id uint) {
+	tx := db.Delete(&Gender{}, id)
+	if tx.Error != nil {
+		fmt.Println(tx.Error)
+		return
+	}
+	fmt.Println("Deleted")
+}
 
 type Test struct {
 	gorm.Model
@@ -73,4 +178,23 @@ type Test struct {
 
 func (t Test) TableName() string {
 	return "MyTest"
+}
+
+func CreateTest(code uint, name string){
+	test := Test{Code: code, Name: name}
+	db.Create(&test)
+}
+
+func GetTests() {
+	tests := []Test{}
+	db.Find(&tests)
+}
+
+
+func SoftDeleteTest(id uint) {
+	db.Delete(&Test{}, id)
+}
+
+func HardDeleteTest(id uint) {
+	db.Unscoped().Delete(&Test{}, id)
 }
